1ea66412d8ad3da62103909f3fc4ac626ad32cb6
yourusername <78161165+Himanshu-Singla@users.noreply.github.com>
2022-09-15 12:39:08 +0530

    added header and css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,94 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import styled from "styled-components";
+import MovieComponent from "./components/MovieComponent";
+import MovieInfoComponent from "./components/MovieInfoComponent";
 
 export const API_KEY = "a9118a3a";
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const AppName = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+const Header = styled.div`
+  background-color: black;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px;
+  font-size: 25px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px 0 #555;
+`;
+const SearchBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  padding: 10px 10px;
+  border-radius: 6px;
+  margin-left: 20px;
+  width: 50%;
+  background-color: white;
+`;
+const SearchIcon = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+const MovieImage = styled.img`
+  width: 48px;
+  height: 48px;
+  margin: 15px;
+`;
+const SearchInput = styled.input`
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  outline: none;
+  margin-left: 15px;
+`;
+const MovieListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 30px;
+  gap: 25px;
+  justify-content: space-evenly; ;
+`;
+const Placeholder = styled.img`
+  width: 120px;
+  height: 120px;
+  margin: 150px;
+  opacity: 50%;
+`;
+
 function App() {
-  return <Container></Container>;
+ 
+  return (
+    <Container>
+      <Header>
+        <AppName>
+          <MovieImage src="/cinema.png" />
+          MovieFlixDB
+        </AppName>
+        <SearchBox>
+          <SearchIcon src="/react-movie-app/search-icon.svg" />
+          <SearchInput
+            placeholder="Search Movie"
+            value={searchQuery}
+            onChange={onTextChange}
+          />
+        </SearchBox>
+      </Header>
+      
+    </Container>
+  );
 }
 
 export default App;
