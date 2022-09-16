a595b6d4cf12864cb8cf0774db3e76a0e5e11380
Abhishek Shakya <71962115+AbhiXShakya@users.noreply.github.com>
2022-09-16 09:00:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ function App() {
 
   const fetchData = async (searchString) => {
     const response = await Axios.get(
-      `https://www.omdbapi.com/?s=${searchString}&apikey=${API_KEY}`
+      `https://www.omdbapi.com/?s=marvel&apikey=${API_KEY}`
     );
     updateMovieList(response.data.Search);
   };
